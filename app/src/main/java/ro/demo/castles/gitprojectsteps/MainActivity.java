22d24cd7226b8359cde72ddb765f0e68999f8641
roxan.popescu <roxan.popescu@gmail.com>
2019-05-02 15:13:50 +0300

    Initial commit

---
+++ app/src/main/java/ro/demo/castles/gitprojectsteps/MainActivity.java
@@ -0,0 +1,15 @@
+package ro.demo.castles.gitprojectsteps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //super code goes here...
+    }
+}
